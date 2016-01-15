e97d8f78b2895387ead3f4f61078eed995d5b9c8
Mathis Moder <m.moder@eightydots.de>
2016-01-15 10:23:11 +0100

    revert to demo-file

--- node-bootstrap.sh
+++ node-bootstrap.sh
@@ -6,32 +6,32 @@ sudo su
 # Just a simple way of checking if we need to install everything
 if [ ! -d "/var/www" ]
 then
-    # Add MongoDB to apt
+	# Add MongoDB to apt
 	apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 	echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 
 
-    # Add nodejs current v4 to apt
+	# Add nodejs current v4 to apt
 	curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
 
-    # Update and begin installing some utility tools
-    apt-get -y update
-    apt-get install -y python-software-properties
-    apt-get install -y vim git subversion curl screen
-    apt-get install -y memcached build-essential
+	# Update and begin installing some utility tools
+	apt-get -y update
+	apt-get install -y python-software-properties
+	apt-get install -y vim git subversion curl	
+	apt-get install -y memcached build-essential
 
-    # Install nodejs
-    apt-get install -y nodejs
+	# Install nodejs
+	apt-get install -y nodejs
 
-    # Install latest stable version of MongoDB
-    apt-get install -y mongodb-org
+	# Install latest stable version of MongoDB
+	apt-get install -y mongodb-org
 
-    # Symlink our host www to the guest /var/www folder
-    ln -s /vagrant/www /var/www
+	# Symlink our host www to the guest /var/www folder
+	ln -s /vagrant/www /var/www
 
-    # Victory!
-    echo "You're all done! Your default node server should now be listening on http://10.0.33.34/. For code, see: node-mongo-vagrant/www/default/server.js."
+	# Victory!
+	echo "You're all done! Your default node server should now be listening on http://10.0.33.34/. For code, see: node-mongo-vagrant/www/default/server.js."
 
-    # Run it
-    screen node /var/www/default/index.js
-fi
+	# Run it
+	node /var/www/default/server.js
+	fi
